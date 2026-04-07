24c47159ce87bc888d5aacfd1701301dd323ff53
luk862-glitch <luk862@newschool.edu>
2026-04-07 19:31:35 -0400

    updated js for when user selects a mode & number but leave topic as "All"

--- assets/main.js
+++ assets/main.js
@@ -13,21 +13,32 @@ let renderItems = () => {
     let topic = document.querySelector('#topic').value
 
 
-    let results = []
-
-    data.forEach(prompt => {
-        if (prompt.mode === mode && 
-            prompt.number === number && 
-            prompt.topic === topic) {
-
 //I wanted the convo prompts to generate randomly each time so I added Math.ramdom () found on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 //but for it to randomly "choose one of the matching prompts" I first have to collect them into an array
 //Found that push method allows me to "add specific elements (aka the 10 prompts that match the user's selection) into an array" https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+    
+    let results = []
+
+    data.forEach(prompt => {
+
+    // If user selects a specific mode & number, but leave topic as "All"
+        if (prompt.mode === mode &&
+            prompt.number === number)
+        {
             results.push(prompt)
         }
+    
+    // If user selects a specific mode & number & topic
+        else if (prompt.mode === mode && 
+            prompt.number === number && 
+            prompt.topic === topic) 
+            
+        {
+            results.push(prompt)
+        }
+
     })
 
-    
     let random = results[Math.floor(Math.random() * results.length)]
 
     let containerEl = document.querySelector('#convo-prompts')
