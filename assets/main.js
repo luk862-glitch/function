29b4f81b59f0a299c49bc6f2f8bdb316cceeaaa9
luk862-glitch <luk862@newschool.edu>
2026-04-08 13:17:18 -0400

    test out "view saved" function & some styling

--- assets/main.js
+++ assets/main.js
@@ -50,9 +50,11 @@ let renderItems = () => {
     <li>
         <h2>${random.prompt}</h2>
         <button id="next" class="next-button">Next</button>
+        <button id="save">Save</button>
     </li>
     `
     containerEl.insertAdjacentHTML('beforeend', itemHtml)
+
 }
 
 
@@ -68,3 +70,27 @@ document.querySelector('#convo-prompts').addEventListener('click', (event) => {
         renderItems()
     }
 })
+
+
+// saved convo prompt button & modal (class site example)
+
+let modalButton = document.querySelector('#modal') // The thing we’re clicking.
+let modalDialog = document.querySelector('#dialog') // Now one for our `dialog`.
+let closeButton = modalDialog.querySelector('button') // Only looking within `modalDialog`.
+
+modalButton.addEventListener('click', () => { // “Listen” for clicks.
+	modalDialog.showModal() // This opens it up.
+})
+
+closeButton.addEventListener('click', () => {
+	modalDialog.close() // And this closes it!
+})
+
+// Listen to *all* clicks, now including the `event` parameter…
+document.addEventListener('click', (event) => {
+	// Only clicks on the page itself behind the `dialog`.
+	if (event.target == document.documentElement) {
+		modalDialog.close() // Close it too then.
+	}
+})
+
