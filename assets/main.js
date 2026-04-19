bdd6e6880ddb6f6a78ca9d30a9c62841af1c8f8f
luk862-glitch <luk862@newschool.edu>
2026-04-19 16:38:18 -0400

    changed mode and topic to fieldsets

--- assets/main.js
+++ assets/main.js
@@ -29,9 +29,9 @@ document.querySelector('#some-form').addEventListener('submit', (event) => {
 
 
 let renderItems = () => {
-    let mode = document.querySelector('#mode').value
+    let mode = document.querySelector('input[name="mode"]:checked').value
     let setting = document.querySelector('input[name="setting"]:checked').value
-    let topic = document.querySelector('#topic').value
+    let topic = document.querySelector('input[name="topic"]:checked').value
 
 
 //I wanted the convo prompts to generate randomly each time so I added Math.ramdom () found on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
